de9f1a738e08c0be51305eb69a2c7932f795a9b5
Cor Corol <corcorol@outlook.com>
2019-03-12 14:29:33 +1000

    XYP1

---
+++ Project1/main.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h> 
+#include <y.tab.h>
+int yylex();
+extern FILE *yyin;
+YYSTYPE yylval;
+int main(int argc, char* argv[]) 
+{
+	fopen_s(&yyin, argv[1], "r");
+
+	int token;  
+	do 
+	{ 
+		token = yylex();   
+		printf("next token: "); 
+		switch (token) 
+		{ 
+			case Literal:                 
+				printf("Literal (%d)\n", yylval.num);                 
+				break;             
+			case IndentifierName:
+				printf("IndentifierName (%s)\n", yylval.name);                 
+				break;             
+			case IF:                 
+				printf("IF\n");                 
+				break;             
+			case ELSE:                 
+				printf("ELSE\n");                 
+				break;             
+			case INT:                 
+				printf("INT\n");                 
+				break;             
+			case BOOL:                 
+				printf("BOOL\n");                 
+				break;             
+			case 0:                 
+				printf("EOF\n");                 
+				break;             
+			default:                 
+				printf("'%c'\n", token);                 
+				break; 
+		}
+	} 
+	while (token != 0);
+
+	return 0;
+}
